a50d7c369cc1ec2fc6a3af5606a56e11118a267d
Leonardo Lemos <leonardo@mixd.com.br>
2018-05-23 18:55:39 -0300

    add a raw post repository implementation

--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -4,13 +4,33 @@ namespace Llemos\BlogEloquentRepository\Repositories;
 
 use Llemos\Blog\Contracts\Repositories\PostRepository as PostRepositoryContract;
 use Llemos\BlogEloquentRepository\Model\Post;
+use Llemos\Blog\Entities\Post as PostEntity;
+use Llemos\Blog\Contracts\Entities\Entity;
 
 class PostRepository extends AbstractRepository implements PostRepositoryContract
 {
-    protected $modelString = Post::class;
+    public function __construct(Post $model)
+    {
+        $this->model = $model;
+    }
 
     public function all()
     {
-        return collect($this->model->with('comments')->get()->toArray());
+        return collect($this->model->all()->map(function ($post) {
+            $entity = new PostEntity($post->toArray());
+            return $entity;
+        }));
+    }
+
+    public function find(string $id): Entity
+    {
+        return new PostEntity($this->model->findOrFail($id)->toArray());
+    }
+
+    public function findBy(string $field, $value): Entity
+    {
+        return new PostEntity(
+            $this->model->where($field, $value)->firstOrFail()->toArray()
+        );
     }
 }
