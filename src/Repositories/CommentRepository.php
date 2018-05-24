c87cf1de3711e91a91d0a44b353d49965db16657
Leonardo Lemos <leonardo@mixd.com.br>
2018-05-24 12:18:22 -0300

    add a comment repository implementation

---
+++ src/Repositories/CommentRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Llemos\BlogEloquentRepository\Repositories;
+
+use Llemos\Blog\Contracts\Repositories\CommentRepository as CommentRepositoryContract;
+use Llemos\BlogEloquentRepository\Model\Comment;
+use Llemos\Blog\Entities\Comment as CommentEntity;
+use Llemos\Blog\Contracts\Entities\Entity;
+
+class CommentRepository extends AbstractRepository implements CommentRepositoryContract
+{
+    public function __construct(Comment $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return collect($this->model->all()->map(function ($comment) {
+            $entity = new CommentEntity($comment->toArray());
+            return $entity;
+        }));
+    }
+
+    public function find(string $id) : Entity
+    {
+        return new CommentEntity($this->model->findOrFail($id)->toArray());
+    }
+
+    public function findBy(string $field, $value) : Entity
+    {
+        return new CommentEntity(
+            $this->model->where($field, $value)->firstOrFail()->toArray()
+        );
+    }
+}
