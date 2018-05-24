a609db607ad08cc4ca4aa71632c979302de862f5
Leonardo Lemos <leonardo@mixd.com.br>
2018-05-24 13:25:59 -0300

    add findByUserId method to CommentRepository

--- src/Repositories/CommentRepository.php
+++ src/Repositories/CommentRepository.php
@@ -6,6 +6,7 @@ use Llemos\Blog\Contracts\Repositories\CommentRepository as CommentRepositoryCon
 use Llemos\BlogEloquentRepository\Model\Comment;
 use Llemos\Blog\Entities\Comment as CommentEntity;
 use Llemos\Blog\Contracts\Entities\Entity;
+use Illuminate\Support\Collection;
 
 class CommentRepository extends AbstractRepository implements CommentRepositoryContract
 {
@@ -17,6 +18,14 @@ class CommentRepository extends AbstractRepository implements CommentRepositoryC
     public function all()
     {
         return collect($this->model->all()->map(function ($comment) {
+            $entity = new CommentEntity($comment->toArray());
+            return $entity;
+        }));
+    }
+
+    public function findByUserId(int $userId) : Collection
+    {
+        return collect($this->model->whereUserId($userId)->get()->map(function ($comment) {
             $entity = new CommentEntity($comment->toArray());
             return $entity;
         }));
